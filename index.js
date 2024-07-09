60712794a138cd0306cedb3f2a08a8802af4ef1f
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-09 14:29:13 +0200

    Created route for handling getting single genre by id param

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.get('/api/genres', (_req, res) => {
     res.send(genres);
 });
 
+app.get('/api/genres/:id', (req, res) => {
+    const selectedGenre = genres.find((singleGenre) => singleGenre.id === parseInt(req.params.id));
+    
+    if(!selectedGenre) return res.status(404).send('Genre with given ID was not found');
+
+    res.send(selectedGenre);
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
