9439f8ca282fc7b606169fc115fd405e92d7912d
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-09 13:03:16 +0200

    Added get route for api/genres that returns all the genres

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
+const genres = [
+    { id: 1, name: 'Action' },  
+    { id: 2, name: 'Horror' },  
+    { id: 3, name: 'Romance' },
+];
+
 app.get('/', (_req, res) => {
     res.send('Hello world');
+});
+
+app.get('/api/genres', (_req, res) => {
+    res.send(genres);
 });
 
 app.listen(port, () => {
