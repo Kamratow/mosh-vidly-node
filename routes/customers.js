c15adb66e6711b7736652e9fde6b1292e18880de
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-11 23:26:58 +0200

    Created customers route to handle all the operations regarding customers

--- routes/customers.js
+++ routes/customers.js
@@ -1,15 +1,87 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const Joi = require("joi");
 
 const router = express.Router();
 
 const Customer = mongoose.model(
   "Customer",
   new mongoose.Schema({
-    isGold: Boolean,
-    name: String,
-    phone: String,
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+    isGold: { type: Boolean, default: false },
+    phone: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
   })
 );
+
+function validateCustomer(customer) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required(),
+    isGold: Joi.boolean().optional(),
+  });
+
+  return schema.validate(customer);
+}
+
+router.get("/", async (_req, res) => {
+  const customers = await Customer.find().sort("name");
+  res.send(customers);
+});
+
+router.get("/:id", async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+
+  if (!customer)
+    return res.status(404).send("Customer with given ID was not found");
+
+  res.send(customer);
+});
+
+router.delete("/:id", async (req, res) => {
+  const customer = await Customer.findByIdAndDelete(req.params.id);
+
+  if (!customer)
+    return res.status(404).send("Customer with given ID was not found");
+
+  res.send(customer);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let newCustomer = new Customer({
+    ...req.body,
+  });
+  newCustomer = await newCustomer.save();
+
+  res.send(newCustomer);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    { ...req.body },
+    { new: true }
+  );
+
+  if (!customer)
+    return res.status(404).send("Customer with given ID was not found");
+
+  res.send(customer);
+});
 
 module.exports = router;
