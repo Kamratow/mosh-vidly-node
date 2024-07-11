c6b3a0c51aa4a6368343dc9fba8f54d887097901
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-11 15:24:52 +0200

    Created mongoose connection to vidly database

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const home = require('./routes/home');
-const genres = require('./routes/genres');
+const mongoose = require("mongoose");
+const express = require("express");
+const home = require("./routes/home");
+const genres = require("./routes/genres");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+mongoose
+  .connect("mongodb://localhost:27017/vidly")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.log("Could not connect to MongoDB...", err));
+
 app.use(express.json());
-app.use('/', home);
-app.use('/api/genres', genres);
+app.use("/", home);
+app.use("/api/genres", genres);
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
