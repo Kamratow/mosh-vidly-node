5e10f33175e46477a83f9ebf78d7070070d58197
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-11 23:53:23 +0200

    Extracted genre modal and validation to a separate file under models directory

--- routes/genres.js
+++ routes/genres.js
@@ -1,28 +1,7 @@
-const mongoose = require("mongoose");
 const express = require("express");
-const Joi = require("joi");
+const { Genre, validate } = require("../models/genre");
 
 const router = express.Router();
-
-const Genre = mongoose.model(
-  "Genre",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
-
-function validateGenre(genre) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  return schema.validate(genre);
-}
 
 router.get("/", async (_req, res) => {
   const genres = await Genre.find().sort("name");
@@ -30,7 +9,7 @@ router.get("/", async (_req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -43,7 +22,7 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByIdAndUpdate(
