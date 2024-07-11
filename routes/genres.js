3d14c7ea75e60a0ce6ffa4ea6bd2a1e796dc29a3
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-11 15:33:56 +0200

    Removed static genres array from genres route as we will be switching now to mongo db vidly database

--- routes/genres.js
+++ routes/genres.js
@@ -16,12 +16,6 @@ const Genre = new mongoose.model(
   })
 );
 
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Horror" },
-  { id: 3, name: "Romance" },
-];
-
 function validateGenre(genre) {
   const schema = Joi.object({
     name: Joi.string().min(3).required(),
