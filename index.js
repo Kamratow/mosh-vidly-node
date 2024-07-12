6eea2b7fd95459ecba78e17862ce0572c410f330
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-12 12:31:11 +0200

    Created movies route and added it to index file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const home = require("./routes/home");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use("/", home);
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
