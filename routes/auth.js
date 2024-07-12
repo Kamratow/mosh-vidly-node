bb39c963fc23c542d2e482706d33431e9b7c3f05
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-13 01:38:33 +0200

    Created auth route to allow users to log in

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const Joi = require("joi");
+const { User } = require("../models/user");
+
+const router = express.Router();
+
+function validate(request) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(request);
+}
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //check if user is already registered
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password.");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
+
+  res.send(true);
+});
+
+module.exports = router;
