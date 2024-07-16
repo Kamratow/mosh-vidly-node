6c754b7ba72bdb465e469e863f3a6439e17b4996
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-16 23:16:00 +0200

    Extracted logging logic from index js to a separate module

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-require("express-async-errors");
 const config = require("config");
 const express = require("express");
-const logger = require("./logger");
 const error = require("./middleware/error");
 
 const app = express();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-process.on("uncaughtException", (ex) => {
-  console.log("We got uncaught exception!");
-
-  logger.log({
-    level: "error",
-    message: ex.message,
-    metadata: ex,
-  });
-  process.exit(1);
-});
-
-process.on("unhandledRejection", (ex) => {
-  console.log("We got unhandled rejection!");
-
-  logger.error(ex.stack, ex);
-  process.exit(1);
-});
 
 // const p = Promise.reject(new Error("Something failed asynchronously!"));
 
---
+++ startup/logging.js
@@ -0,0 +1,22 @@
+require("express-async-errors");
+const logger = require("./logger");
+
+module.exports = function () {
+  process.on("uncaughtException", (ex) => {
+    console.log("We got uncaught exception!");
+
+    logger.log({
+      level: "error",
+      message: ex.message,
+      metadata: ex,
+    });
+    process.exit(1);
+  });
+
+  process.on("unhandledRejection", (ex) => {
+    console.log("We got unhandled rejection!");
+
+    logger.error(ex.stack, ex);
+    process.exit(1);
+  });
+};
