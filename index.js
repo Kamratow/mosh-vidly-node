33623dcf851d9ad7aa525f88632d9f20e08aed71
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-16 23:30:23 +0200

    Extracted config logic from index js to a separate module

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const config = require("config");
 const express = require("express");
 const error = require("./middleware/error");
 
@@ -7,13 +6,9 @@ const app = express();
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/config")();
 
 // const p = Promise.reject(new Error("Something failed asynchronously!"));
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exitCode = 1;
-}
 
 const port = process.env.PORT || 3000;
 
