929a9c3215b59ff955345ecb1644628b9ba825c5
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-16 23:49:23 +0200

    Adjusted logging levels for different transports

--- logger.js
+++ logger.js
@@ -5,10 +5,12 @@ module.exports = winston.createLogger({
   transports: [
     new winston.transports.File({
       filename: "logfile.log",
+      level: "error",
     }),
-    new winston.transports.Console(),
+    new winston.transports.Console({ level: "info" }),
     new winston.transports.MongoDB({
       db: "mongodb://localhost:27017/vidly",
+      level: "error",
       options: {
         useUnifiedTopology: true,
       },
