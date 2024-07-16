0f41469498010e223c35cf8c510d6915944aec99
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-16 17:59:26 +0200

    Removed previous routes setup leftover from index js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ process.on("unhandledRejection", (ex) => {
   process.exit(1);
 });
 
-const p = Promise.reject(new Error("Something failed asynchronously!"));
+// const p = Promise.reject(new Error("Something failed asynchronously!"));
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
@@ -38,15 +38,6 @@ mongoose
   .connect("mongodb://localhost:27017/vidly")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.log("Could not connect to MongoDB...", err));
-
-app.use(express.json());
-app.use("/", home);
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
 
 // Custom error middleware
 app.use(error);
