881b4b7f5c30da36ab0b2a7923b2dcf26767939d
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-18 00:26:51 +0200

    Created integration tests for auth middleware

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -10,8 +10,8 @@ describe("/api/genres", () => {
   });
 
   afterEach(async () => {
+    await server.close();
     await Genre.deleteMany({});
-    server.close();
   });
 
   describe("GET /", () => {
