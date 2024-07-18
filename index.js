e50428cb951a62bd8fd8120c000141da1c67cff8
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-18 23:43:48 +0200

    Created prod startup file where both helmet and compression middlewares are applied

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
+require("./startup/prod")(app);
 
 // const p = Promise.reject(new Error("Something failed asynchronously!"));
 
