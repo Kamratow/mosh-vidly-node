4607fc7dded8b7db0cc6f1a05a256ae21a18348e
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-18 11:37:35 +0200

    Created intergration test for not passing the movieId to returns post endpoint

--- routes/returns.js
+++ routes/returns.js
@@ -6,6 +6,10 @@ const router = express.Router();
 router.post("/", auth, async (req, res) => {
   if (!req.body.customerId)
     return res.status(400).send("CustomerId not provided");
+
+  if (!req.body.movieId) return res.status(400).send("MovieId not provided");
+
+  res.status(200).send("return success");
 });
 
 module.exports = router;
