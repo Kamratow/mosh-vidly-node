c05069e652ed967fa8609b27f9b3e13d31cc95c3
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-18 23:12:26 +0200

    Moved rental lookup logic to a static method available in Rental model

--- models/rental.js
+++ models/rental.js
@@ -1,62 +1,68 @@
 const Joi = require("joi");
 const mongoose = require("mongoose");
 
-const Rental = mongoose.model(
-  "Rental",
-  new mongoose.Schema({
-    customer: {
-      type: new mongoose.Schema({
-        name: {
-          type: String,
-          required: true,
-          minLength: 5,
-          maxLength: 50,
-        },
-        isGold: {
-          type: Boolean,
-          default: false,
-        },
-        phone: {
-          type: String,
-          required: true,
-          minLength: 5,
-          maxLength: 50,
-        },
-      }),
-      required: true,
-    },
-    movie: {
-      type: new mongoose.Schema({
-        title: {
-          type: String,
-          required: true,
-          trim: true,
-          minLength: 5,
-          maxLenght: 255,
-        },
-        dailyRentalRate: {
-          type: Number,
-          required: true,
-          min: 0,
-          max: 255,
-        },
-      }),
-      required: true,
-    },
-    dateOut: {
-      type: Date,
-      required: true,
-      default: Date.now,
-    },
-    dateReturned: {
-      type: Date,
-    },
-    rentalFee: {
-      type: Number,
-      min: 0,
-    },
-  })
-);
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 50,
+      },
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 50,
+      },
+    }),
+    required: true,
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 5,
+        maxLenght: 255,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  },
+});
+
+rentalSchema.statics.lookup = function (customerId, movieId) {
+  return this.findOne({
+    "customer._id": customerId,
+    "movie._id": movieId,
+  });
+};
+
+const Rental = mongoose.model("Rental", rentalSchema);
 
 function validateRental(rental) {
   const schema = Joi.object({
--- routes/returns.js
+++ routes/returns.js
@@ -14,10 +14,7 @@ router.post("/", [auth, validate(validateReturn)], async (req, res) => {
   //     movie: { _id: req.body.movieId },
   //   });
 
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
   if (!rental) return res.status(404).send("Rental not found.");
 
